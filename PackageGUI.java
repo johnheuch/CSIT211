722911a8b24a762de1a19ca5cd37ed2b850753c7
Tyler Haigis <tyler.haigis@gmail.com>
2017-12-03 13:53:26 -0500

    Update PackageGUI.java

--- PackageGUI.java
+++ PackageGUI.java
@@ -1,3 +1,8 @@
+//Author:				John Heuchan, Tyler Haigis, Rafay Khurram
+//Program Name:				PackageGUI.java
+//Program Description:			This GUI program include the main method for final project.				
+//IDE used:				Eclipse IDE for Java Developers, Version: Oxygen Release (4.7.0)
+
 import javax.swing.*;
 import java.io.IOException;
 import java.util.*;
@@ -6,38 +11,33 @@ public class PackageGUI
 {
 	public static void main(String[] args) throws IOException, ClassNotFoundException
 	{
+		int PNumber =  0; 
 		String FName = "";
 		String LName = "";
-		//String contactNo="";
-		String returnAddress = "";
 		String Address = "";
-		String priority = "";
-		
-		
-		//=================== CODE FOR PICK UP PACKAGES================================================
-		PickUp PU = new PickUp();
-		PU.addPickUp(new PackageInfo(123,"Jim","Baker","123 AnyStreet","NewTown","DC","20000"));
-		PU.addPickUp(new PackageInfo(120,"Tim","Jones","1203 That Street","NewTown","DC","20000"));
-		PU.addPickUp(new PackageInfo(121,"Tony","Smith","1273 Lost Way","NewTown","DC","20000"));		
-		PackageInfo tester = PU.find(120);
-		tester.SetDate("3/1/18");
-	
-		PackageInfo tester2 = PU.find(123);
-		PU.addPickUpAfter(tester2, new PackageInfo(122,"Jim","Jones","888 Easy Street","NoWork","DC","20000"));
-		
-		//=============================================================================================
-		//PackageInfo = packInfo = new PackageInfo();
-		// Need package info from John's code
+		String city = "";
+		String state = "";
+		String zip = "";
+		String date = "";
+		String shipType = "";
 		
 		int choice = 0;
 		String option = "";
 		String output = "";
+		Random rand = new Random();
 		
-		Queue normalQueue = new Queue();
-		Queue priorityQueue = new Queue();
+		String pickUp = "";  // used to select which package to pick up
+		String cancel = ""; // used to select which delivery to cancel
+		
+		Queue shippingQueue = new Queue();
+		Queue pickupQueue = new Queue();
 		Package temp;
 		LinkedList AllQueue = new LinkedList();
 		
+		System.out.println(" Package Shipping Program. \n");
+		System.out.println(" Transactions:");
+		System.out.println(" -----------------");
+
 		while (choice == 0)
 		{
 			option = JOptionPane.showInputDialog(null, "Choose Option\n\tShip\n\tPick Up\n\tCancel Shipment\n\tView\n\tExit");
@@ -45,79 +45,120 @@ public class PackageGUI
 			// Ship
 			if (option.equalsIgnoreCase("Ship"))
 			{
+				Delivery del = new Delivery(); // used to create and store deliveries
+				PackageInfo packIn = new PackageInfo(PNumber, FName, LName, Address, city, state, zip, date);
+				
+				PNumber = rand.nextInt(300) + 1; // Random Package number for each package
 				FName = JOptionPane.showInputDialog(null, "First Name");
 				LName = JOptionPane.showInputDialog(null, "Last Name");
-				//contactNo=JOptionPane.showInputDialog(null, "Contact Number");
-				returnAddress = JOptionPane.showInputDialog(null, "Return Address");
 				Address = JOptionPane.showInputDialog(null, "Address");
-				priority = JOptionPane.showInputDialog(null,"Shipment Method [normal/priority]");
+				city = JOptionPane.showInputDialog(null, "City");
+				state = JOptionPane.showInputDialog(null, "State");
+				zip = JOptionPane.showInputDialog(null, "Zip");
+				date = JOptionPane.showInputDialog(null, "Date (DD/MM/YY)");
+				shipType = JOptionPane.showInputDialog(null,"Shipment Method [Pick Up/Delivery]");
 				
-				temp = new Package(FName, LName, returnAddress, Address, priority);
+				temp = new Package(PNumber, FName, LName, Address, city, state, zip, date, shipType);
 				
-				if (temp.priorityCheck())
+				if (temp.packageCheck())
 				{
-					priorityQueue.enqueue(temp); // Queue for Priority Shipping
+					pickupQueue.enqueue(temp);	  // Queue for pickups
 				}
 				else
 				{
-					normalQueue.enqueue(temp);  // Queue for Normal Shipping
+					shippingQueue.enqueue(temp); // Queue for deliveries
+					del.addDelivery(packIn);
+					del.addDeliveryAfter(packIn, packIn);
 				}
-			}
-			
-			//======================== STILL NEED CODE FOR PICK UP=============================================
-			else if (option.equalsIgnoreCase("Pick Up"))
-			{
 				
 			}
 			
-			
-			//=================================================================================================
+			// Pick up
+			else if (option.equalsIgnoreCase("Pick Up"))
+			{
+				PickUp pu = new PickUp();  // used to create and store pick ups
+				
+				if(!pickupQueue.isEmpty()) // Displays packages listed as pick ups
+				{
+					for(int i=0; i<pickupQueue.size(); i++)
+					{
+						temp = (Package) pickupQueue.front();
+						output+=temp.toString();
+					}
+				}
+
+				//======================= Need to remove from list/dequeue==============================
+				JOptionPane.showMessageDialog(null, "Pick Ups:\n"+output);
+				output = "";
+				
+				pickUp = JOptionPane.showInputDialog(null, "Enter Package Number to Pickup");
+				pu.find(PNumber);
+				pu.CheckNumber(PNumber);
+				pu.iterator();
+				pu.save(pickUp);
+				
+				if (pu.CheckNumber(PNumber) == true)
+				{
+					pickupQueue.dequeue();
+					System.out.println(" Package Number: " + PNumber + ", has been picked up. ");
+				}
+			}
+			  //=========================================================================================
 			
 			// Cancel Shipment
-			else if (option.equalsIgnoreCase("Cancel Shipment"))
+			else if (option.equalsIgnoreCase("Cancel Shipment")) 
 			{
-				// View before cancel
-				if(!priorityQueue.isEmpty())
+				// View shipments before cancel
+				Delivery canc = new Delivery();
+			
+				if(!shippingQueue.isEmpty()) // displays packages listed at delivery
 				{
-					for(int i=0; i<priorityQueue.size(); i++)
+					for(int i = 0; i<shippingQueue.size(); i++)
 					{
-						temp = (Package) priorityQueue.front();
+						temp = (Package) shippingQueue.front();
 						output+=temp.toString();
 					}
 				}
-				if(!normalQueue.isEmpty())
-				{
-					for(int i = 0; i<normalQueue.size(); i++)
-					{
-						temp = (Package) normalQueue.front();
-						output+=temp.toString();
-					}
-				}
-				JOptionPane.showMessageDialog(null, "Select which Package? \n" +output); // change to select PNumber? 
+				
+				//======================= Need to remove from list/dequeue=====================================================
+				JOptionPane.showMessageDialog(null, "Select which shipment to cancel: \n" +output);
 				output ="";
+				
+				cancel = JOptionPane.showInputDialog(null, "Enter Package Number to Cancel");
+				canc.find(PNumber);
+				canc.CheckNumber(PNumber);
+				canc.iterator();
+				canc.save(cancel);
+				
+				if(canc.CheckNumber(PNumber) == true)
+				{
+					shippingQueue.dequeue();
+					System.out.println(" Package Number : " + PNumber + ", has been canceled.");
+				}
 			}
+			   //=================================================================================================================
 			
 			
 			// View
 			else if (option.equalsIgnoreCase("View"))
 			{
-				if(!priorityQueue.isEmpty())
+				if(!pickupQueue.isEmpty())  // displays packages listed as pick up
 				{
-					for(int i=0; i<priorityQueue.size(); i++)
+					for(int i=0; i<pickupQueue.size(); i++)
 					{
-						temp = (Package) priorityQueue.front();
+						temp = (Package) pickupQueue.front();
 						output+=temp.toString();
 					}
 				}
-				if(!normalQueue.isEmpty())
+				if(!shippingQueue.isEmpty()) // displays packages listed as delivery
 				{
-					for(int i=0; i<normalQueue.size(); i++)
+					for(int i=0; i<shippingQueue.size(); i++)
 					{
-						temp = (Package) normalQueue.front();
+						temp = (Package) shippingQueue.front();
 						output+=temp.toString();
 					}
 				}
-				JOptionPane.showMessageDialog(null, "Viewing\n"+output);
+				JOptionPane.showMessageDialog(null, "Viewing:\n"+output);
 				output = "";
 			}
 			
@@ -181,64 +222,51 @@ class Queue
 
 class Package
 {
+	protected int PNumber;
+	protected String city;
+	protected String state;
+	protected String zip;
+	protected String date;
 	protected String FName;
 	protected String LName;
-	protected String returnAddress;
 	protected String Address;
-	protected String priority;
+	protected String shipType;
 	
-	Package (String a, String b, String c, String d, String e)
+	Package (int a ,String b, String c, String d, String e, String f, String g, String h, String i) 
 	{
-		FName = a;
-		LName = b;
-		returnAddress = c;
+		PNumber = a;
+		FName = b;
+		LName = c;
 		Address = d;
-		priority = e;
+		city = e;
+		state = f;
+		zip = g; 
+		date = h;
+		shipType = i;
 	}
-	/*
-	PackageInfo.GetFName();
-	PackageInfo.GetLName();
-	PackageInfo.GetReturnAddress();
-	PackageInfo.GetAddress();
-	PackageInfo.GetPriority();
-	*/
-	
+
+	public int getPNumber(){return PNumber;}
 	public String getFName(){return FName;}
 	public String getLName(){return LName;}
-	public String getReturnAddress(){return returnAddress;}
+	public String getCity(){return city;}
+	public String getState(){return state;}
+	public String getZip(){return zip;}
+	public String getDate(){return date;}
 	public String getAddress(){return Address;}
-	public String getPriority(){return priority;}
+	public String getShiptype(){return shipType;}
 	
-	public boolean priorityCheck()
+	public boolean packageCheck()
 	{
-		if(getPriority().equalsIgnoreCase("priority"))
+		if(getShiptype().equalsIgnoreCase("pick up"))
 			return true;
 		else
 			return false;
 	}
-	
+
 	public String toString()
 	{
-		return "First Name: " + getFName() + ", Last Name: " + getLName() + ", Return Address: " + getReturnAddress() + ", Destination: " +getAddress() + ", Shipping Method: " + getPriority() + "\n";
+		return "Package Number: " + getPNumber() +", First Name: " + getFName() + ", Last Name: " 
+	+ getLName() + ", Destination: " +getAddress() + ", City: " + getCity() 
+	+ ", State: " + getState() + ", Zip: " + getZip() + ", Date: " + getDate() + ", Shipping Method: " + getShiptype() + "\n";
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
